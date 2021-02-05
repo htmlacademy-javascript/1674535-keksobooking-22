04ea32e8738164c3a26ee1de85e9e10d7388b155
Tatyana Komarova <komarik_slon@mail.ru>
2021-02-05 11:21:57 +0300

    Исправила замечания

--- js/main.js
+++ js/main.js
@@ -1,41 +1,15 @@
-const getRandomInteger = (min, max) => {
-  if (typeof min != 'number' || typeof max != 'number' || min > max || min < 0){
-    alert('Задайте корректный диапазон. Границы не могут быть отрицательными. Нижняя граница должна быть меньше или равна верхней');
-    return undefined;
-  }
-
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  if (min == max){
-    return min;
-  }
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const getRandomFloat = (min, max, numberOfDecimal) => {
-  if (typeof min != 'number' || typeof max != 'number' || typeof numberOfDecimal != 'number' || min > max || min < 0){
-    alert('Задайте корректный диапазон. Границы не могут быть отрицательными. Нижняя граница должна быть меньше или равна верхней');
-    return undefined;
-  }
-
-  if (min == max){
-    return min;
-  }
-
-  return (Math.random() * (max - min) + min).toFixed(numberOfDecimal);
-}
-
-const getRandomArrayElement = (elements) => {
-  return elements[getRandomInteger(0, elements.length - 1)];
-};
+'use strict';
 
 const OBJECTS_COUNT = 10;
 const MAX_GUESTS_COUNT = 8;
 const MAX_ROOMS_COUNT = 6;
 const MIN_PRICE = 1500000;
 const MAX_PRICE = 15000000;
+const MIN_X = 35.65000;
+const MAX_X = 35.70000;
+const MIN_Y = 139.70000;
+const MAX_Y = 139.80000;
+const NUMBER_OF_DECIMAL_X_Y = 5;
 
 const CHECK_TIME = [
   '12:00',
@@ -78,30 +52,67 @@ const FEATURES = [
   'conditioner',
 ];
 
-const getRandomLendhtArray = (elements)=> {
-  let newArrayLength = getRandomInteger(0, elements.length);
-  if (newArrayLength == 0){
+const getRandomInteger = (min, max) => {
+  if (min > max || min < 0){
+    alert('Задайте корректный диапазон. Границы не могут быть отрицательными. Нижняя граница должна быть меньше или равна верхней');
+    return undefined;
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  if (min === max){
+    return min;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const getRandomFloat = (min, max, numberOfDecimal) => {
+  if (min > max || min < 0){
+    alert('Задайте корректный диапазон. Границы не могут быть отрицательными. Нижняя граница должна быть меньше или равна верхней');
+    return undefined;
+  }
+
+  if (min === max){
+    return min;
+  }
+
+  return (Math.random() * (max - min) + min).toFixed(numberOfDecimal);
+}
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomInteger(0, elements.length - 1)];
+};
+
+const getRandomLengthForArray = (length) => {
+  return getRandomInteger(0, length);
+}
+
+const createRandomArray = (elements) => {
+  const newArrayLength = getRandomLengthForArray(elements.length);
+  if (newArrayLength === 0){
     return null;
   }
 
   let tempArray = new Array();
   tempArray[0] = getRandomInteger(0, elements.length - 1);
-  if (newArrayLength !=1 ){
+  if (newArrayLength !==1 ){
     for (let i = 1; i < newArrayLength; i++){
       let isNew;
       do {
         isNew = true;
         let randomNumber = getRandomInteger(0, elements.length - 1);
         for (let j = 0; j < i; j++){
-          if (randomNumber == tempArray[j]){
+          if (randomNumber === tempArray[j]){
             isNew = false;
           }
         }
-        if (isNew == true){
+        if (isNew === true){
           tempArray[i] = randomNumber;
         }
       }
-      while (isNew == false)
+      while (isNew === false)
     }
   }
 
@@ -121,8 +132,8 @@ const createAutor = () => {
 
 const createLocation = () => {
   return{
-    x: getRandomFloat(35.65000, 35.70000, 5),
-    y: getRandomFloat(139.70000, 139.80000, 5),
+    x: getRandomFloat(MIN_X, MAX_X, NUMBER_OF_DECIMAL_X_Y),
+    y: getRandomFloat(MIN_Y, MAX_Y, NUMBER_OF_DECIMAL_X_Y),
   };
 }
 
@@ -136,9 +147,9 @@ const createOffer = (newLocation) => {
     guests: getRandomInteger(0, MAX_GUESTS_COUNT),
     checkin: getRandomArrayElement(CHECK_TIME),
     checkout: getRandomArrayElement(CHECK_TIME),
-    features: getRandomLendhtArray(FEATURES),
+    features: createRandomArray(FEATURES),
     description: getRandomArrayElement(DESCRIPTION_PHRASES),
-    photos: getRandomLendhtArray(OBJECT_IMAGES),
+    photos: createRandomArray(OBJECT_IMAGES),
   };
 }
 
